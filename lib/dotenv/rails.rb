b57d8be346e9d32816e10c8cfd1c772a0161bb8d
Benjamin Behr <benny@digitalbehr.de>
2014-10-16 14:34:16 +0200

    configure spring after initializing the framework

--- lib/dotenv/rails.rb
+++ lib/dotenv/rails.rb
@@ -8,15 +8,17 @@ end
 module Dotenv
   class Railtie < Rails::Railtie
     config.before_configuration { load }
+    config.after_initialize { configure_spring }
 
     # Public: Load dotenv
     #
     # This will get called during the `before_configuration` callback, but you
     # can manually call `Dotenv::Railtie.load` if you needed it sooner.
     def load
-      base_path = Rails.root
-      env_files = [base_path.join('.env'), base_path.join(".env.#{Rails.env}")]
       Dotenv.load *env_files
+    end
+
+    def configure_spring
       Spring.watch *env_files if defined?(Spring)
     end
 
@@ -24,6 +26,12 @@ module Dotenv
     # instance, which means `Kernel#load` gets called here. We don't want that.
     def self.load
       instance.load
+    end
+
+    private
+    def env_files
+      base_path = Rails.root
+      [base_path.join('.env'), base_path.join(".env.#{Rails.env}")]
     end
   end
 end
